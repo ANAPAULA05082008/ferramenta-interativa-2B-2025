307e5d8b89844a921493c7a06cee4e678b8019fe
ANAPAULA05082008 <a.batista05@escola.pr.gov.br>
2025-09-17 16:23:42 -0300

    17/09/2025

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,24 @@
 function vria criacartao(categoria, pregunta, resposta) {
     console.log(categoria, pregunta, resposta)
+    let container = document.getElementById('contianer')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+            <h3>${categoria}</h3>
+            <div class="cartao__conteudo__pergunta">
+                  <p>${pregunta}</p> 
+            </div>
+            <div class="cartao__conteudo__resposta">
+                   <p> ${resposta}</p> 
+            </div>
+    </div>
+    `
+
+
+
+
+
 }
+
+
